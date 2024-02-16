d20ecf7c76dd00a85af52a91c745b6a6d12e39bc
mohamed <mohammedromail94@gmail.com>
2024-02-16 21:41:58 +0200

    f

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './App.css';
+import SignUP from './componant/SignUP';
+
+function App() {
+  return (
+    <div>
+     <SignUP/>
+    </div>
+  );
+}
+
+export default App;
